f7d1fd6c21ae18f183c7517c7240e23465d1944d
AlenChernick <crazyguy771@gmail.com>
2022-10-15 19:27:17 +0300

    Orders data table added + pagging(pagination) + sorting + page header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ category, title }) => {
+  return (
+    <div className='mt-12 mb-10 md:my-0 md:mb-10'>
+      <p className='text-gray-400'>{category}</p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900'>{title}</p>
+    </div>
+  );
 };
 
 export default Header;
