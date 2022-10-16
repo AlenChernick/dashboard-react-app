e5ba8dcb0108b24571d67635f4a7e02d13840584
AlenChernick <crazyguy771@gmail.com>
2022-10-16 03:43:52 +0300

    added color-mapping chart

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  ColumnSeries,
+  Category,
+  Tooltip,
+  Legend,
+  RangeColorSettingsDirective,
+  RangeColorSettingDirective,
+} from '@syncfusion/ej2-react-charts';
+import { colorMappingData, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis, rangeColorMapping } from '../../data/dummy';
+import { Header } from '../../components';
 
 const ColorMapping = () => {
-  return <div>ColorMapping</div>;
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Color Mapping' title='USA CLIMATE - WEATHER BY MONTH' />
+      <ChartComponent
+        id='color-mapping-chart'
+        height='420px'
+        primaryXAxis={ColorMappingPrimaryXAxis}
+        primaryYAxis={ColorMappingPrimaryYAxis}
+        legendSettings={{ mode: 'Range', background: 'white' }}
+        tooltip={{ enable: true }}
+      >
+        <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={colorMappingData[0]}
+            xName='x'
+            yName='y'
+            name='USA'
+            type='Column'
+            cornerRadius={{ topLeft: 10, topRight: 10 }}
+          />
+        </SeriesCollectionDirective>
+        <RangeColorSettingsDirective>
+          {rangeColorMapping.map((item, index) => (
+            <RangeColorSettingDirective key={index} {...item} />
+          ))}
+        </RangeColorSettingsDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default ColorMapping;
