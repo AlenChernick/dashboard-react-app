55ecd2a6f6198073ba1091b25ab1ee519ace68f9
AlenChernick <crazyguy771@gmail.com>
2022-10-16 04:39:03 +0300

    added cart menu

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useStateContext } from '../contexts/ContextProvider';
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
+const Button = ({ icon, bgColor, color, size, text, borderRadius, width }) => {
+  const { setIsClicked, initialState } = useStateContext();
   return (
-    <button type='button' style={{ backgroundColor: bgColor, color, borderRadius }} className={`test-${size} p-3 hover:drop-shadow-xl`}>
+    <button
+      onClick={() => setIsClicked(initialState)}
+      type='button'
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`w-${width} text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {icon}
       {text}
     </button>
   );
