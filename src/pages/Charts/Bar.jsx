470307cd6d6ced4210b1dc29a0c3523055515a94
AlenChernick <crazyguy771@gmail.com>
2022-10-16 03:10:35 +0300

    added bar chart

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Category,
+  Tooltip,
+  Legend,
+  DataLabel,
+  ColumnSeries,
+} from '@syncfusion/ej2-react-charts';
+import { barCustomSeries, barPrimaryYAxis, barPrimaryXAxis } from '../../data/dummy';
+import { Header } from '../../components';
 
-const Bar = () => {
-  return <div>Bar</div>;
+const Area = () => {
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Bar' title='Olympic Medal Counts - RIO"' />
+      <ChartComponent
+        id='bar-chart'
+        height='420px'
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+      >
+        <Inject services={[Legend, Tooltip, Category, DataLabel, ColumnSeries]} />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
-export default Bar;
+export default Area;
